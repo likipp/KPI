306ebb2df410537388b7fdacfa9643db01fcb714
likipp <likipp@163.com>
2019-11-17 21:32:57 +0800

    序列化角色成员

--- apps/rbac/serializers/role_serializer.py
+++ apps/rbac/serializers/role_serializer.py
@@ -1,4 +1,7 @@
+from collections import OrderedDict
+
 from rest_framework import serializers
+
 from ..models import Role
 
 
@@ -9,6 +12,14 @@ class RoleListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Role
         fields = '__all__'
+
+    def to_representation(self, instance):
+        ret = super(RoleListSerializer, self).to_representation(instance)
+        if not isinstance(instance, OrderedDict):
+            member_set = instance.userProfile_roles.all()
+            members = [{'id': user.id, 'name': user.name, 'username': user.username} for user in member_set]
+            ret['members'] = members
+            return ret
 
 
 class RoleModifySerializer(serializers.ModelSerializer):
