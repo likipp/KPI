ec9827ee8d2734a3f9152ff8e9a07653674723f6
likipp <likipp@163.com>
2019-11-22 16:17:52 +0800

    icon serializers设置trim_whitespace

--- apps/rbac/serializers/menu_serializer.py
+++ apps/rbac/serializers/menu_serializer.py
@@ -6,6 +6,8 @@ class MenuSerializer(serializers.ModelSerializer):
     """
     菜单序列化
     """
+    # 因为使用iconFont在iView中需要加空格,到后端会删除空格.所以需要设置trim_whitespace=False.官方资料中有介绍
+    icon = serializers.CharField(max_length=50, allow_blank=True, allow_null=True, trim_whitespace=False)
 
     class Meta:
         model = Menu
--- apps/rbac/views/menu_view.py
+++ apps/rbac/views/menu_view.py
@@ -20,3 +20,4 @@ class MenuViewSet(ModelViewSet, TreeAPIView):
 
 class MenuTreeView(TreeAPIView):
     queryset = Menu.objects.all()
+
